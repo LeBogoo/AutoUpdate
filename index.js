fb381050280d83e9c21686aa3c7f965d034bf8f5
LeBogo <lebogomc@gmail.com>
2021-07-26 19:07:31 +0200

    Added auto updater

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 require('dotenv').config();
 const Discord = require('discord.js');
-const fs = require('fs');
 const { promisify } = require('util');
 const exec = promisify(require('child_process').exec);
 
-
 const client = new Discord.Client();
-
 
 client.on('ready', () => {
     console.log("Bot is ready!")
@@ -16,5 +13,18 @@ client.on('message', (msg) => {
     const { guild, channel, author, cleanContent } = msg;
     console.log(`${guild.name}, #${channel.name} | ${author.username}: ${cleanContent}`);
 });
+
+// Check for updates on start
+async function updateCheck() {
+    const hash = await exec('git rev-parse HEAD');
+    await exec('git pull');
+    const newHash = await exec('git rev-parse HEAD');
+
+    // Exit bot. Systemd will restart automatically.
+    if (hash !== newHash) process.exit();
+}
+
+setInterval(updateCheck, 1000 * 60);
+updateCheck();
 
 client.login(process.env.TOKEN);
