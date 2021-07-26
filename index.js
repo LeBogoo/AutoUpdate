a7fb31c38299b8949db30143a194b9ceb7556f87
LeBogo <lebogomc@gmail.com>
2021-07-26 19:46:48 +0200

    Added URL to commit embed

--- index.js
+++ index.js
@@ -15,10 +15,14 @@ client.on('message', async (msg) => {
         const info = (await exec('git log -1')).stdout.split("\n").filter(e => e !== '');
         const versionEmbed = new Discord.MessageEmbed();
 
+        console.log(info[0]);
         var commit = info[0].substr(0, 14); // Get "commit" + 7 chars of hash
         commit = commit.charAt(0).toUpperCase() + commit.slice(1); // Capitalize first char
 
         versionEmbed.setTitle(commit);
+
+        var commitURL = (await (await exec('git remote get-url origin'))).stdout.replace(".git", `/commit/${info[0].substr(7)}`);
+        versionEmbed.setURL(commitURL);
 
         var author = info[1].replace('Author: ', '').split(' <')[0];
         versionEmbed.addField('Author:', author)
