2ce07350935e692c3ba549dea9faa16e688b0720
LeBogo <lebogomc@gmail.com>
2021-07-26 19:00:14 +0200

    Added preparation for auto update

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 require('dotenv').config();
 const Discord = require('discord.js');
+const fs = require('fs');
+const { promisify } = require('util');
+const exec = promisify(require('child_process').exec);
+
 
 const client = new Discord.Client();
 
 
 client.on('ready', () => {
     console.log("Bot is ready!")
-})
+});
 
 client.on('message', (msg) => {
     const { guild, channel, author, cleanContent } = msg;
     console.log(`${guild.name}, #${channel.name} | ${author.username}: ${cleanContent}`);
-})
-
+});
 
 client.login(process.env.TOKEN);
